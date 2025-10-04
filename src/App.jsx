2e343cbeb1bef2fa04defcc8c9451af939073af4
SabiDev <kelechiemmanuel888@gmail.com>
2025-10-04 23:19:25 +0100

    typing2

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
 import { Footer, Navbar } from "./components";
 
-import Projects from "./pages/Projects.jsx";
-import Contact from "./pages/Contact.jsx";
-import Home from "./pages/Home.jsx";
-import About from "./pages/About.jsx";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 const App = () => {
     return (
--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ import CTA from "./CTA";
 import Alert from "./Alert";
 import Loader from "./Loader";
 import Navbar from "./Navbar";
-import Footer from "./Footer";
+import Footer from "./Footer.jsx";
 import HomeInfo from "./HomeInfo";
 
 export {
