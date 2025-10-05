4847d35494015e48037e6a87ab68b426ed52412c
Akwara Kelechi Emmanuel <137418423+Kellysabi@users.noreply.github.com>
2025-10-05 12:13:48 -0700

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -190,7 +190,7 @@ export const socialLinks = [
     {
         name: 'Contact',
         iconUrl: contact,
-        link: "/contact",
+        link: "contact",
     },
     {
         name: 'GitHub',
